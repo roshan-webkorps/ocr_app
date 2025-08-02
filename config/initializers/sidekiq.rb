9727438e9026fdc563ffcfdcfb8421fe0bb039bd
roshan-webkorps <roshan@webkorps.com>
2025-08-02 19:12:53 +0530

    update initialize

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,18 @@
 require "sidekiq"
 require "sidekiq/api"
 
-require_relative "../../app/workers/document_processor_worker"
-
-require "sidekiq/rails" if defined?(Rails)
-
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDIS_URL"] || "redis://localhost:6379/0" }
 
   config.on(:startup) do
-    Rails.application.eager_load! if defined?(Rails)
+    Rails.logger.info "Sidekiq startup - loading workers..."
+
+    Dir[Rails.root.join("app/workers/**/*.rb")].each do |file|
+      require file
+      Rails.logger.info "Loaded worker file: #{file}"
+    end
+
+    Rails.logger.info "All workers loaded!"
   end
 
   config.error_handlers << proc do |ex, ctx_hash|
